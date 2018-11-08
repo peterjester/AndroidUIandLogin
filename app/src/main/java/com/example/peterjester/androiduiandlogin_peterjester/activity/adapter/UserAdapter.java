a0a514e3c761ea072739a4a5059aafdd23df11c9
Peter <peter.j.jester@gmail.com>
2018-11-08 17:39:46 -0500

    adding in layout adapter

--- app/src/main/java/com/example/peterjester/androiduiandlogin_peterjester/activity/adapter/UserAdapter.java
+++ app/src/main/java/com/example/peterjester/androiduiandlogin_peterjester/activity/adapter/UserAdapter.java
@@ -1,7 +1,15 @@
 package com.example.peterjester.androiduiandlogin_peterjester.activity.adapter;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.peterjester.androiduiandlogin_peterjester.R;
+import com.example.peterjester.androiduiandlogin_peterjester.activity.model.entity.UserProfile;
 
 import java.util.List;
 
@@ -11,5 +19,39 @@ public class UserAdapter extends ArrayAdapter {
         super(context, resource, objects);
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
 
+        View listItemView = convertView;
+
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.custom_list_item, parent, false);
+        }
+
+
+        UserProfile user = (UserProfile) getItem(position);
+
+        TextView nameView = (TextView) listItemView.findViewById(R.id.nameView);
+        nameView.setText(user.getName());
+
+        TextView surnameView = (TextView) listItemView.findViewById(R.id.surnameView);
+        surnameView.setText(user.getSurname());
+
+        TextView usernameView = (TextView) listItemView.findViewById(R.id.usernameView);
+        usernameView.setText(user.getUsername());
+
+        TextView  birthdayView = (TextView) listItemView.findViewById(R.id.birthdayView);
+        birthdayView.setText(user.getBirthday());
+
+        TextView phoneView = (TextView) listItemView.findViewById(R.id.phoneView);
+        phoneView.setText(user.getName());
+
+        TextView emailView = (TextView) listItemView.findViewById(R.id.emailView);
+        emailView.setText(user.getName());
+
+        return listItemView;
+
+    }
 }
